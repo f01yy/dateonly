efa26d3c8c85fd7f05aaa5aead297a6a3cf99dff
Renat Ibraev <renibraew@yandex.ru>
2025-08-18 16:53:20 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Telegram: any;
+  }
 }
 
-export default App
+function App() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+    tg.ready(); // сообщает Telegram, что приложение готово
+    setUser(tg.initDataUnsafe?.user);
+  }, []);
+
+  return (
+      <div>
+        {user ? (
+          <div>
+            <p>Привет, {user.first_name}!</p>
+            <p>Твой Telegram ID: {user.id}</p>
+          </div>
+        ) : (
+          <p>Загрузка данных пользователя...</p>
+        )}
+      </div>
+  );
+}
+
+export default App;
