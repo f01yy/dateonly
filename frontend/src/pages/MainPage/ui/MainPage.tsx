5ad02dc0fba15e4c9eb6b650093acd87a0f2761e
Renat Ibraev <renibraew@yandex.ru>
2025-08-20 16:43:44 +0300

    feature: mock tg data + start backend testing

--- frontend/src/pages/MainPage/ui/MainPage.tsx
+++ frontend/src/pages/MainPage/ui/MainPage.tsx
@@ -1,43 +1,20 @@
-import { useEffect, useState } from 'react'
+import { retrieveLaunchParams } from '@telegram-apps/sdk-react'
 
 export const MainPage = () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [user, setUser] = useState<any>(null)
-
-  useEffect(() => {
-    const tg = window.Telegram.WebApp
-    tg.ready() // сообщает Telegram, что приложение готово
-    setUser(tg.initDataUnsafe?.user)
-  }, [])
-
-  const sendDataToBot = () => {
-    const tg = window.Telegram.WebApp
-    // можно отправлять строку JSON с любыми данными
-    tg.sendData(
-      JSON.stringify({
-        action: 'like',
-        userId: user?.id,
-      })
-    )
-  }
-
+  const params = retrieveLaunchParams()
+  console.log(params)
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center p-6">
       <h1 className="text-2xl font-bold mb-4">🚀 Telegram Dating Mini App</h1>
 
-      {user ? (
+      {params.tgWebAppData?.user ? (
         <div className="bg-white shadow p-4 rounded-xl w-full max-w-sm">
-          <p className="text-lg">Привет, {user.first_name}!</p>
-          <p className="text-sm text-gray-500 mb-4">
-            Твой Telegram ID: {user.id}
+          <p className="text-lg">
+            Привет, {params.tgWebAppData?.user.first_name}!
           </p>
-
-          <button
-            onClick={sendDataToBot}
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg shadow"
-          >
-            Отправить данные боту
-          </button>
+          <p className="text-sm text-gray-500 mb-4">
+            Твой Telegram ID: {params.tgWebAppData?.user.id}
+          </p>
         </div>
       ) : (
         <p>Загрузка данных пользователя...</p>
